4e90c17c60ce323723d42d0ba93eff293df21b13
Sam Davidoff <sam@mfbtech.com>
2025-06-08 23:29:47 +0000

    fix scrolling logic

--- src/app/games/containShip.ts
+++ src/app/games/containShip.ts
@@ -1,5 +1,5 @@
 // Based on contain_ship() from orig/Sources/Play.c:394-457
-import { LEFTMARG, RIGHTMARG, SHIPHT, VIEWHT, SCRWTH } from '@/screen/constants'
+import { LEFTMARG, RIGHTMARG, TOPMARG, BOTMARG, SHIPHT, VIEWHT, SCRWTH } from '@/screen/constants'
 
 export type ContainmentResult = {
   shipx: number
@@ -39,51 +39,63 @@ export function containShip(
 ): ContainmentResult {
   let { shipx, shipy, dx, dy } = ship
   let { screenx, screeny } = screen
-  const { worldwidth, worldwrap } = world
+  const { worldwidth, worldheight, worldwrap } = world
 
-  // Handle horizontal scrolling with soft boundaries
-  if (shipx < LEFTMARG) {
-    screenx += shipx - LEFTMARG
-    shipx = LEFTMARG
-  } else if (shipx > RIGHTMARG) {
-    screenx += shipx - RIGHTMARG
-    shipx = RIGHTMARG
+  // From contain_ship() in Play.c:394-457
+  /* hold ship inside of planet; if outside of screen, move screen rect */
+  
+  if (shipx < LEFTMARG)
+  {   screenx += shipx - LEFTMARG;
+      shipx = LEFTMARG;
   }
-
-  // Handle world boundaries for non-wrapping worlds
-  if (!worldwrap && screenx < 0) {
-    shipx += screenx
-    screenx = 0
+  else if (shipx > RIGHTMARG)
+  {   screenx += shipx - RIGHTMARG;
+      shipx = RIGHTMARG;
   }
-  if (!worldwrap && screenx > worldwidth - SCRWTH) {
-    shipx += screenx - (worldwidth - SCRWTH)
-    screenx = worldwidth - SCRWTH
+  if (!worldwrap && screenx < 0)
+  {   shipx += screenx;
+      screenx = 0;
   }
-
-  // Handle wrapping worlds
-  if (worldwrap) {
-    if (screenx >= worldwidth) {
-      screenx -= worldwidth
-    } else if (screenx < 0) {
-      screenx += worldwidth
-    }
+  if (!worldwrap && screenx > worldwidth - SCRWTH)
+  {   shipx += screenx - (worldwidth - SCRWTH);
+      screenx = worldwidth - SCRWTH;
   }
-
-  // Hard screen edge constraints for ship
-  if (shipx < SHIPHT) {
-    shipx = SHIPHT
-    dx = 0
-  } else if (shipx > SCRWTH - SHIPHT) {
-    shipx = SCRWTH - SHIPHT
-    dx = 0
+  
+  if(screenx >= worldwidth)
+      screenx -= worldwidth;
+  else if(screenx < 0)
+      screenx += worldwidth;
+      
+  if (shipy < TOPMARG)
+  {   screeny += shipy - TOPMARG;
+      shipy = TOPMARG;
   }
-
-  if (shipy < SHIPHT) {
-    shipy = SHIPHT
-    dy = 0
-  } else if (shipy > VIEWHT - SHIPHT) {
-    shipy = VIEWHT - SHIPHT
-    dy = 0
+  else if (shipy > BOTMARG)
+  {   screeny += shipy - BOTMARG;
+      shipy = BOTMARG;
+  }
+  if (screeny < 0)
+  {   shipy += screeny;
+      screeny = 0;
+  }
+  else if (screeny > worldheight - VIEWHT)
+  {   shipy += screeny - (worldheight - VIEWHT);
+      screeny = worldheight - VIEWHT;
+  }
+  
+  // Note: screenr, screenb, globalx, globaly, on_right_side are calculated elsewhere
+  
+  if (shipy < SHIPHT || shipy > VIEWHT-SHIPHT)
+  {
+      dy = 0;
+      shipy = (shipy < SHIPHT ? SHIPHT : VIEWHT - SHIPHT);
+      // Note: Original has contain_ship() recursion here
+  }
+  if (shipx < SHIPHT || shipx > SCRWTH-SHIPHT)
+  {
+      dx = 0;
+      shipx = (shipx < SHIPHT ? SHIPHT : SCRWTH - SHIPHT);
+      // Note: Original has contain_ship() recursion here
   }
 
   return {
