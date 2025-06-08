ecea4422dcc13104a9775d63e77c47d894e16980
Sam Davidoff <sam@mfbtech.com>
2025-06-08 22:51:01 +0000

    feat: add planet Redux slice

---
+++ src/planet/planetSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PlanetState } from './types'
+
+const initialState: PlanetState = {
+  worldwidth: 0,
+  worldheight: 0,
+  worldwrap: false,
+  shootslow: 0,
+  xstart: 0,
+  ystart: 0,
+  planetbonus: 0,
+  gravx: 0,
+  gravy: 0,
+  numcraters: 0,
+  lines: [],
+  bunkers: [],
+  fuels: [],
+  craters: []
+}
+
+export const planetSlice = createSlice({
+  name: 'planet',
+  initialState,
+  reducers: {}
+})
+
+export default planetSlice.reducer
