dcbcea8780abce745b76024813a1e8e22ede9db3
Sam Davidoff <sam@mfbtech.com>
2025-07-23 16:20:04 +0000

    Revert "fix oneClose"
    This reverts commit cafd22f4cd161398f48a7dff78cb5359bcbd5014.

--- src/walls/init/oneClose.ts
+++ src/walls/init/oneClose.ts
@@ -84,6 +84,8 @@ export function oneClose(
     )
     if (index !== -1) {
       replaceWhite2(targetX, targetY, targetX, targetY, ht, data)
+    } else {
+      addWhite(targetX, targetY, ht, data)
     }
   }
 
@@ -95,7 +97,7 @@ export function oneClose(
     ht: number,
     data: number[]
   ) => {
-    // C code: for (wh=whites; wh < whites + numwhites && (wh->y != targety || wh->x != targetx || wh.ht >= ht); wh++);
+    // C code: for (wh=whites; wh < whites + numwhites && (wh->y != targety || wh->x != targetx || wh->ht >= ht); wh++);
     const index = currentWhites.findIndex(
       wh => wh.x === targetX && wh.y === targetY && wh.ht < ht
     )
@@ -117,6 +119,8 @@ export function oneClose(
           ...currentWhites.slice(index + 1)
         ]
       }
+    } else {
+      addWhite(x, y, ht, data)
     }
   }
 
