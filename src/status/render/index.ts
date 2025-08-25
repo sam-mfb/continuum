4158ccf92e15f170651bdedbf95de1c05467b249
Sam Davidoff <sam@mfbtech.com>
2025-08-25 01:36:24 +0000

    Implement basic status bar writers (writeInt, writeLong, writeStr)

--- src/status/render/index.ts
+++ src/status/render/index.ts
@@ -2,5 +2,11 @@
  * Status bar rendering functions
  */
 
+// Low-level functions
 export { drawDigit } from './drawDigit'
 export { sbarClear, sbarClearOptimized } from './sbarClear'
+
+// Basic writers
+export { writeInt } from './writeInt'
+export { writeLong } from './writeLong'
+export { writeStr } from './writeStr'
