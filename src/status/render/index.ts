af106cbfbf88470fb7255958b70a2c097f15750c
Sam Davidoff <sam@mfbtech.com>
2025-08-25 01:38:49 +0000

    Implement specific field writers for status bar

--- src/status/render/index.ts
+++ src/status/render/index.ts
@@ -10,3 +10,11 @@ export { sbarClear, sbarClearOptimized } from './sbarClear'
 export { writeInt } from './writeInt'
 export { writeLong } from './writeLong'
 export { writeStr } from './writeStr'
+
+// Specific field writers
+export { writeFuel } from './writeFuel'
+export { writeScore } from './writeScore'
+export { writeBonus } from './writeBonus'
+export { writeLives } from './writeLives'
+export { writeLevel } from './writeLevel'
+export { writeMessage } from './writeMessage'
